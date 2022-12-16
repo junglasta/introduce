191a0210903c6abbddda7e1d98f751c5fe986166
jung_la_sta <jung_ls_sta@naver.com>
2022-12-17 00:18:20 +0900

    사전인터뷰 DB노출란 초기형태

--- src/App.js
+++ src/App.js
@@ -7,34 +7,46 @@ function App() {
     <div className="App" id="top">
       <Header></Header>
       <Worksswiper></Worksswiper>
-      <Preinterview ></Preinterview>      
-      <Preinterview contentid="me"></Preinterview>
-      <Preinterview contentid="preInterview"></Preinterview>
+      <Preinterview contentid="portfolio" title="포트폴리오" num="0"></Preinterview>      
+      <Preinterview contentid="me" title="그누보드" num="1"></Preinterview>
+      <Preinterview contentid="preInterview" title="기획서" num="0"></Preinterview>
       <Footer></Footer>
     </div>
   );
 }
 
-const Preinterview = (props) => {
-  const interviewjson = [{
-    subject : '코딩보다 이게 더 어려워',
-    content : '일단 만나봅시다! 사장'
-  },
-  {
-    subject : '사진인터뷰의 용도를 잘 이해하고 기입하셔야합니다.',
-    content : `그들이 나에게 요구하는것이 나한테 있는지 여부파악하고 잘 써야합니다.
-    없는 거는 없다 있는거는 있다. 
-    나만 나중에 피곤한 일이 없도록 써야합니다.`
-  }]
+const Preinterview = ( props ) => {
+  const interviewjson = [
+    [
+      {
+      subject : '0번 배열 제목1',
+      content : '일단 만나봅시다! 사장'
+      },
+      {
+        subject : '0번 배열 제목2',
+        content : `그들이 나에게 요구하는것이 나한테 있는지 여부파악하고 잘 써야합니다.`
+      }
+    ],
+    [
+      {
+      subject : '1번 배열 제목1',
+      content : '일단 만나봅시다! 사장'
+      },
+      {
+        subject : '1번 배열 제목2',
+        content : `없는 거는 없다 있는거는 있다. 나만 나중에 피곤한 일이 없도록 써야합니다.`
+      }
+    ]
+  ]
 
   return(
     
     <section className="section py-5 text-center" id={props.contentid}>
-        <h3>규칙위에 틀을 깨는 개발자가 되겠습니다.</h3>
+        <h3>{props.title}</h3>
         <div className="py-5 container-md text-start">
           <ul>
-             {
-              interviewjson.map(function(value, index){
+            {
+              interviewjson[props.num].map(function(value, index){
                   //this조심할것
                   return(
                     <li  key={'interview'+index}>
@@ -44,7 +56,7 @@ const Preinterview = (props) => {
                   )
               }
               )
-             }
+            }
           </ul>
         </div>
     </section>
